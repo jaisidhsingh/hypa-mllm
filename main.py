71bd1d1b0c1ba85de3a5709c6521f3dba5a44079
jaisidhsingh <jaisidhsingh@gmail.com>
2024-11-20 14:05:04 +0100

    debug llama-3.2 mllm

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from torch.utils.data import DataLoader
 from src.configs.data_configs import data_configs
 from src.data import FeatureAlignmentDataset
-from src.model import run_model_unit_tests
-
-run_model_unit_tests()
+from src.model import run_model_unit_tests, MLLM
+import torch
+# run_model_unit_tests()
 
 
 dataset = FeatureAlignmentDataset(**data_configs.pretraining_dataset_configs)
@@ -14,3 +14,34 @@ print(images)
 print(prompts)
 print(answers)
 print(image_positions)
+
+
+image_size = 224
+
+model = MLLM(
+    llm="llama-3.2",
+    vision_tower="vanilla_vit_b16",
+    connector_type="linear",
+    connector_hidden_dims=[],
+    device="cuda"
+)
+
+
+images = torch.randn((1, 3, image_size, image_size))
+input_text = "A cat sat on the"
+input_text_enc = model.tokenizer(input_text, return_tensors="pt")
+input_text_ids = input_text_enc.input_ids
+
+target_text = " mat."
+target_text_enc = model.tokenizer(target_text, return_tensors="pt")
+target_text_ids = target_text_enc.input_ids
+
+full_text = input_text + target_text
+full_text_enc = model.tokenizer(full_text, return_tensors="pt")
+full_text_ids = full_text_enc.input_ids
+attention_mask = full_text_enc.attention_mask
+
+labels = torch.full((1, model.num_patches+full_text_ids.shape[1]), fill_value=-100, dtype=torch.long)
+labels[:, model.num_patches+input_text_ids.shape[1]:] = target_text_ids
+
+output = model(full_text_ids, images, attention_mask, labels)
--- src/model/model_tests.py
+++ src/model/model_tests.py
@@ -6,10 +6,8 @@ from src.model.mllm import MLLM
 from src.model.hypernetworks import HyperNetwork
 
 
-# class TestingMLLM(parameterized.TestCase):
-class TestingMLLM():
-    # def setUp(self):
-    def __init__(self):
+class TestingMLLM(parameterized.TestCase):
+    def setUp(self):
         self.batch_size = 4
         self.image_size = 224
         self.num_patches = 197
@@ -62,43 +60,41 @@ class TestingMLLM():
             print("Error in forward pass.")
             print(err)
 
-        # self.assertEqual(forward_pass_correctly_done, True)
+        self.assertEqual(forward_pass_correctly_done, True)
         
     @torch.no_grad()
     def test_hypernetwork(self):
         hypnet_works = False
-        # try:
-        param_shapes = [[768, 768], [768]]
-        hypnet = HyperNetwork(
-            param_shapes=param_shapes,
-            cond_emb_dim=8,
-            num_cond_embs=3,
-            image_embed_dims=[768],
-            hidden_layer_factors=[]
-        )
-        print("Hypernetwork initisialised correctly.")
+        try:
+            param_shapes = [[768, 768], [768]]
+            hypnet = HyperNetwork(
+                param_shapes=param_shapes,
+                cond_emb_dim=8,
+                num_cond_embs=3,
+                image_embed_dims=[768],
+                hidden_layer_factors=[]
+            )
+            print("Hypernetwork initisialised correctly.")
 
-        weights, biases = hypnet([0, 1, 2], 768)
-        hypnet_works = True
+            weights, biases = hypnet([0, 1, 2], 768)
+            hypnet_works = True
 
-        print("Hypernetwork forward pass correctly executed.")
-        print("Obtained params for connects have dims:")
-        print(weights.shape, biases.shape)
+            print("Hypernetwork forward pass correctly executed.")
+            print("Obtained params for connects have dims:")
+            print(weights.shape, biases.shape)
         
-        # except:
-        #     print("Error in hypernetwork.")
+        except:
+            print("Error in hypernetwork.")
         
-        # self.assertEqual(hypnet_works, True)
+        self.assertEqual(hypnet_works, True)
 
     @torch.no_grad()
     def test_full_hypermllm(self):
         """
         Not implemented yet.
         """
-        # self.assertEqual(True, True)
+        self.assertEqual(True, True)
 
 
 def run_model_unit_tests():
-    # absltest.main(__name__)
-    testing = TestingMLLM()
-    testing.test_mllm_forward_pass()
+    absltest.main(__name__)
