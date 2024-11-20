98131dcf775dd9db8dec9980401f538584088c72
jaisidhsingh <jaisidhsingh@gmail.com>
2024-11-20 13:22:50 +0100

    test dataloader

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from src.configs.data_configs import data_configs
 from src.data import FeatureAlignmentDataset
+from torch.utils.data import DataLoader
 
 
 dataset = FeatureAlignmentDataset(**data_configs.pretraining_dataset_configs)
+loader = DataLoader(dataset, batch_size=4, collate_fn=dataset.collate_fn)
+images, prompts, answers, image_positions = next(loader)
 
-print(len(dataset))
-sample = dataset[0]
-for item in sample:
-    print(type(item))
+print(images)
+print(prompts)
+print(answers)
+print(image_positions)
