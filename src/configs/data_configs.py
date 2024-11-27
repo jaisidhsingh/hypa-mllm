9dfa2877eaf34398d9e47a2c55da38cba5324442
jaisidhsingh <jaisidhsingh@gmail.com>
2024-11-27 21:35:30 +0100

    add dummy training code

--- src/configs/data_configs.py
+++ src/configs/data_configs.py
@@ -6,9 +6,12 @@ data_configs = SimpleNamespace(**{})
 data_configs.global_data_folder = "/home/mila/s/sparsha.mishra/scratch/tinyllava-pretrain"
 
 data_configs.pretraining_dataset_configs = {
-    "image_folder": f"{data_configs.global_data_folder}/tinyllava_558k/images",
-    "annotations_path": f"{data_configs.global_data_folder}/tinyllava_558k/blip_laion_cc_sbu_558k.json",
-    "image_token": "<image>",
-    "transform": None,
-    "tokenizer": None 
+    "train": {
+        "image_folder": f"{data_configs.global_data_folder}/tinyllava_558k/images",
+        "annotations_path": f"{data_configs.global_data_folder}/tinyllava_558k/blip_laion_cc_sbu_558k.json",
+        "image_token": "<image>",
+        "transform": None,
+        "tokenizer": None 
+    },
+    "val": {} # TODO: add cc-3m 300k here as the val set.
 }
--- src/training/train_mllm.py
+++ src/training/train_mllm.py
@@ -1,5 +1,63 @@
-"""
-Ideally have one file for
-1. default MLLM training
-2. hyper-MLLM training
-"""
+from transformers import TrainingArguments, Trainer
+from torch.utils.data import DataLoader
+import argparse
+import wandb
+import math
+
+from src.data import FeatureAlignmentDataset
+from src.model import MLLM
+from src.training.trainers import TrainerForMLLM
+from src.configs.data_configs import data_configs
+
+
+def main(args):
+    train_dataset = FeatureAlignmentDataset(**data_configs.pretraining_dataset_configs)
+    model = MLLM(
+        llm="llama-3.2",
+        vision_tower="vanilla_vit_b16",
+        connector_type="linear",
+        connector_hidden_dims=[],
+        device=args.device
+    )
+    model.train()
+
+    wandb.init(project=args.experiment_type, config=vars(args))
+
+    total_steps = math.ceil(len(train_dataset) / args.batch_size) * args.num_epochs
+
+    training_args = TrainingArguments(
+        output_dir=args.train_log_folder,
+        num_train_epochs=args.num_epochs,
+        per_device_train_batch_size=args.batch_size,
+        learning_rate=args.learning_rate,
+        report_to="wandb",
+        run_name=args.experiment_name,
+        logging_steps=1,
+        eval_strategy="steps",
+        eval_steps=total_steps,
+    )
+    trainer = TrainerForMLLM(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        data_collator=train_dataset.collate_fn
+    )
+    
+    try:
+        trainer.train()
+    finally:
+        wandb.finish()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--experiment_type", type=str, required=True)
+    parser.add_argument("--experiment_name", type=str, required=True)
+    parser.add_argument("--train_log_folder", type=str, required=True)
+    parser.add_argument("--num_epochs", type=int, default=3)
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--learning_rate", type=float, default=5e-5)
+    parser.add_argument("--device", type=str, default="cuda")
+
+    args = parser.parse_args()
+    main(args)
