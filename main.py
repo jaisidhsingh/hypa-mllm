8464ba4c4a0b82e7a0a939a4af95460bfe422437
jaisidhsingh <jaisidhsingh@gmail.com>
2024-11-28 10:40:23 +0100

    take debug out of train file

--- main.py
+++ main.py
@@ -1,26 +1,45 @@
+import os
+import math
+import wandb
+import argparse
 from torch.utils.data import DataLoader
-from src.configs.data_configs import data_configs
+from transformers import TrainingArguments, Trainer
+
+from src.model import MLLM
 from src.data import FeatureAlignmentDataset
-from src.model import run_model_unit_tests, MLLM
-import torch
-# run_model_unit_tests()
+from src.training.trainers import TrainerForMLLM
+from src.configs.data_configs import data_configs
 
 
-dataset = FeatureAlignmentDataset(**data_configs.pretraining_dataset_configs["train"])
-loader = DataLoader(dataset, batch_size=4, collate_fn=dataset.collate_fn)
-batch = next(iter(loader))
+def main(args):
+    model = MLLM(
+        llm="llama-3.2",
+        vision_tower="vanilla_vit_b16",
+        connector_type="linear",
+        connector_hidden_dims=[],
+        device=args.device
+    )
+    model.train()
 
-for k, v in batch.items():
-    print(k, v.shape)
+    train_dataset_config = data_configs.pretraining_dataset_configs["train"]
+    train_dataset_config.update({"transform": model.image_transform, "tokenizer": model.tokenizer})
+    train_dataset = FeatureAlignmentDataset(**train_dataset_config)
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, collate_fn=train_dataset.collate_fn)
 
-model = MLLM(
-    llm="llama-3.2",
-    vision_tower="vanilla_vit_b16",
-    connector_type="linear",
-    connector_hidden_dims=[],
-    device="cpu"
-)
-model.train()
+    batch = next(iter(train_loader))
+    output = model(**batch)
+    print(output.keys())
 
-output = model(**batch)
-print(output.keys())
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--device", type=str, default="cuda")
+    parser.add_argument("--experiment_name", type=str, default="mllm_training_test_0")
+    parser.add_argument("--experiment_type", type=str, default="mllm")
+    parser.add_argument("--train_log_folder", type=str, default="../../logs")
+    parser.add_argument("--num_epochs", type=int, default=1)
+    parser.add_argument("--batch_size", type=int, default=4)
+    parser.add_argument("--learning_rate", type=float, default=3e-4)
+
+    args = parser.parse_args()
+    main(args)
+
