0580e0766b2a77b95fe0d388d967bae22d329fb7
jaisidhsingh <jaisidhsingh@gmail.com>
2024-12-02 11:49:21 +0100

    add seed and submit job

--- run_training.sh
+++ run_training.sh
@@ -16,7 +16,7 @@ conda activate /home/mila/s/sparsha.mishra/.conda/envs/sparse
 
 ulimit -Sn $(ulimit -Hn)
 
-python3 pyfile \
+python3 $pyfile \
     --random_seed=0 \
     --batch_size=32 \
     --learning_rate=1e-3 \
